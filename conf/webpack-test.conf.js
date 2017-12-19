19d80fcb89da060cfc0cdaa11e4be1ddddb50294
sn00zzzy <snoozzzy7@gmail.com>
2017-12-20 00:01:46 +0200

    Master:     First commit > deployed project > added:         - basic structure;         - basic styles;         - casio pic;         - digital font like in calcs;

---
+++ conf/webpack-test.conf.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loaders: [
+          'json-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 50000,
+          mimetype: 'application/font-woff',
+          name: './fonts/[name].[ext]'
+        },
+      }
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {},
+      debug: true
+    })
+  ],
+  devtool: 'source-map',
+  externals: {
+    'react/lib/ExecutionEnvironment': 'true',
+    'react/lib/ReactContext': 'true'
+  }
+};
