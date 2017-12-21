57de350218f37c7e023aebd06c424d692d71eca1
sn00zzzy <snoozzzy7@gmail.com>
2017-12-22 00:33:39 +0200

    CALC-for-show:    logic ready;

--- src/app/calculator.js
+++ src/app/calculator.js
@@ -1,27 +1,104 @@
 import React, {Component} from 'react';
-import {Controls} from './controls';
-import {Buttons} from './buttons';
 
 export class Calculator extends Component {
   state = {
-    displayValue: '0'
+    displayValue: '0',
+    checkForNewInput: false,
+    value: null,
+    control: null
   }
   inputDigit(digit) {
+    const {displayValue, checkForNewInput} = this.state;
+    if (checkForNewInput) {
+      this.setState({
+        displayValue: String(digit),
+        checkForNewInput: false
+      });
+    } else {
+      this.setState({
+        displayValue: displayValue === '0' ? String(digit) : displayValue + digit
+      });
+    }
+  }
+  inputDot() {
+    const {displayValue, checkForNewInput} = this.state;
+    if (checkForNewInput) {
+      this.setState({
+        displayValue: '.',
+        checkForNewInput: false
+      });
+    } else if (displayValue.indexOf('.') === -1) {
+      this.setState({
+        displayValue: displayValue + '.',
+        checkForNewInput: false
+      });
+    }
+  }
+  inputClear() {
     const {displayValue} = this.state;
     this.setState({
-      displayValue: String(digit)
+      displayValue: '0'
+    });
+  }
+  toggleSign() {
+    const {displayValue} = this.state;
+    this.setState({
+      displayValue: displayValue.charAt(0) === '-' ? displayValue.substr(1) : '-' + displayValue
+    });
+  }
+  mathControl(nextControl) {
+    const {displayValue, control, value} = this.state;
+    const nextValue = parseFloat(displayValue);
+    const calculations = {
+      '+': (prevValue, nextValue) => prevValue + nextValue,
+      '-': (prevValue, nextValue) => prevValue - nextValue,
+      '%': (prevValue) => parseFloat(prevValue / 100),
+      'x': (prevValue, nextValue) => prevValue * nextValue,
+      '÷': (prevValue, nextValue) => prevValue / nextValue,
+      '=': (prevValue, nextValue) => nextValue
+    }
+    if(value == null) {
+      this.setState({
+        value: nextValue
+      });
+    } else if (control) {
+      const currentInput = value || 0;
+      const calculated = calculations[control](currentInput, nextValue);
+
+      this.setState({
+        value: calculated,
+        displayValue: String(calculated)
+      });
+    }
+    this.setState({
+      checkForNewInput: true,
+      control: nextControl
     });
   }
   render() {
     const {displayValue} = this.state;
     return (
       <div>
-        <div>
-          {displayValue}
-        </div>
-        <button onClick={() => this.inputDigit(11)}>11</button>
-        <Buttons onClick={() => this.inputDigit(1)}/>
-        <Controls/>
+        <div>{displayValue}</div>
+        <button onClick={() => this.inputClear()}>AC</button>
+        <button onClick={() => this.inputDigit(0)}>0</button>
+        <button onClick={() => this.inputDigit(1)}>1</button>
+        <button onClick={() => this.inputDigit(2)}>2</button>
+        <button onClick={() => this.inputDigit(3)}>3</button>
+        <button onClick={() => this.inputDigit(4)}>4</button>
+        <button onClick={() => this.inputDigit(5)}>5</button>
+        <button onClick={() => this.inputDigit(6)}>6</button>
+        <button onClick={() => this.inputDigit(7)}>7</button>
+        <button onClick={() => this.inputDigit(8)}>8</button>
+        <button onClick={() => this.inputDigit(9)}>9</button>
+        <button onClick={() => this.inputDot()}>.</button>
+        <button onClick={() => this.toggleSign()}>±</button>
+        <button onClick={() => this.mathControl('+')}>+</button>
+        <button onClick={() => this.mathControl('-')}>-</button>
+        <button onClick={() => this.mathControl('*')}>*</button>
+        <button onClick={() => this.mathControl('÷')}>÷</button>
+        <button onClick={() => this.mathControl('%')}>%</button>
+        <button onClick={() => this.mathControl('=')}>=</button>
       </div>
     );
   }
